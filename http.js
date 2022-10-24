c5f9c54394a27f904d4cc98e75f70726e38faa81
Isaac Zhang <isaac-z@outlook.com>
2022-10-24 17:30:07 +0800

    update

--- http.js
+++ http.js
@@ -1,20 +1,20 @@
-const http = require('http');
+const http = require('http')
 
 // 创建服务器实例
-const server = http.createServer();
+const server = http.createServer()
 
 //绑定request事件，监听客户端请求
 server.on('request', (req, res) => {
-    const url = req.url;
-    const method = req.method;
-    console.log(`url is ${url}, method is ${method}`);
+    const url = req.url
+    const method = req.method
+    console.log(`url is ${url}, method is ${method}`)
 
     //response内容
-    const str = `your url is ${url}, method is ${method}`;
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    res.end(str);
-});
+    const str = `your url is ${url}, method is ${method}`
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    res.end(str)
+})
 
 server.listen(8080, () => {
     console.log('server running...')
-});
+})
