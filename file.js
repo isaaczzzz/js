55ab278ea82ed489cf2ab4042286ac8f1fe56bc9
Isaac Zhang <isaac-z@outlook.com>
2022-10-24 17:30:53 +0800

    update

--- file.js
+++ file.js
@@ -1,14 +1,14 @@
-fs = require('fs');
-path = require('path');
+fs = require('fs')
+path = require('path')
 
 fs.readFile(path.join(__dirname, '/files/1.txt'), 'utf-8', function(err, dataStr) {
     if (err) {
-        console.log('读取文件失败' + err.message);
+        console.log('读取文件失败' + err.message)
     } else {
-        console.log('读取文件成功: ' + dataStr);
+        console.log('读取文件成功: ' + dataStr)
     }
-});
+})
 
 // fs.writeFile('./files/2.txt', 'aasd', function(err) {
-//     console.log(err);
+//     console.log(err)
 // })
